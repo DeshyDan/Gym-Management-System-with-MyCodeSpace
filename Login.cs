3e2d42f5bdb3953be1966d34672b1829652b9e00
Deshy Dan <deshy733@gmail.com>
2023-09-04 11:55:22 +0200

    Added the login form

--- Login.cs
+++ Login.cs
@@ -10,11 +10,16 @@ using System.Windows.Forms;
 
 namespace Gym_Management_System_with_MyCodeSpace
 {
-    public partial class Form1 : Form
+    public partial class Login : Form
     {
-        public Form1()
+        public Login()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
